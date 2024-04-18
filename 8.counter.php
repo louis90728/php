6f87b642b607cc504e935de2992fab98522f34ed
louis90728 <64579044+louis90728@users.noreply.github.com>
2024-04-19 00:37:49 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["counter"]))
-        $_SESSION["counter"]=1;
+    session_start(); // 啟動PHP會話（session）
+    if (!isset($_SESSION["counter"])) // 檢查是否已經設置了會話變數 
+        $_SESSION["counter"]=1; // 如果沒有，將計數器設置為1。
     else
-        $_SESSION["counter"]++;
+        $_SESSION["counter"]++; // 將計數器值增加1
 
-    echo "counter=".$_SESSION["counter"];
-    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+    echo "counter=".$_SESSION["counter"]; // 在網頁上輸出計數器的值
+    echo "<br><a href=9.reset_counter.php>重置counter</a>"; // 點擊該連結時將重置計數器的值
 ?>
