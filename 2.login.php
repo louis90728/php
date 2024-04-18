1c8a3a29106628b5c97f0f7e410f45a2e25022ba
louis90728 <64579044+louis90728@users.noreply.github.com>
2024-04-18 13:07:53 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];
-    echo "<br>";
-    echo $_POST["pwd"];
+    echo $_POST["id"]; // 顯示2.login.html的帳號 id
+    echo "<br>"; // 換行符號
+    echo $_POST["pwd"]; // 顯示2.login.html的密碼 pwd
 ?>
