0d103de17f29f416eb493c3eed52764c2a90b5fe
louis90728 <64579044+louis90728@users.noreply.github.com>
2024-04-19 00:39:40 +0800

    Update 9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,8 @@
 <?php
-    session_start();
-    unset($_SESSION["counter"]);
-    echo "counter重置成功....";
-    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
+    session_start(); // 啟動會話
+    unset($_SESSION["counter"]); // 刪除會話中的計數器
+    echo "counter重置成功...."; // 輸出重置成功訊息
+    echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>"; // 重定向到指定頁面
+
 
 ?>
