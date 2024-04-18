90956dbd2a57ba14e0b7e514fdcb286e21c5fb56
louis90728 <64579044+louis90728@users.noreply.github.com>
2024-04-18 12:56:30 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,8 +1,8 @@
 <html>
-    <head><title>html+php</title></head>      
+    <head><title>html+php</title></head>  // 設定網站的標題為html+php 
     <body>
-        <?php echo "Hello world!"; ?> 
-        <h1>html</h1>
+        <?php echo "Hello world!"; ?> // 在網頁上顯示Hello world!這句話
+        <h1>html</h1> // 將文字html顯示為最高級標題
         <?php echo "Hello world!"; ?> 
         <h1>html</h1>
         <?php echo "Hello world!"; ?> 
