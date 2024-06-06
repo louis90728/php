c0d36605538a35975b5b7d140dbed5dc5d2fa4b1
louis90728 <64579044+louis90728@users.noreply.github.com>
2024-06-06 21:14:54 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    unset($_SESSION["id"]);
-    echo "登出成功....";
-    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
+    session_start(); // 啟動 session 功能
+    unset($_SESSION["id"]); // 刪除名為 "id" 的 session 變數
+    echo "登出成功....";  // 輸出登出成功的訊息
+    echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>"; // 設定網頁重新導向，延遲 3 秒後導向到 2.login.html
 
 ?>
